6265e6d9d800b5f8fbb95b7953c6296420d24d81
Carol Leung <carol.carl.leung@gmail.com>
2022-01-12 18:21:43 +0100

    chore: removes button from foo page

--- src/pages/Foo/index.tsx
+++ src/pages/Foo/index.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import Layout from "@theme/Layout";
-import { Button } from "@cawo88/component-library-example";
 
 export default function Foo(): JSX.Element {
-  return (
-    <Layout title="Foo">
-      <Button label="Button" primary />
-      Foo page
-    </Layout>
-  );
+  return <Layout title="Foo">Foo page</Layout>;
 }
